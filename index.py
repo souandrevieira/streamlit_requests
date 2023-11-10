26c6ff28c4a5648a1d66f98343f7b5c159cda3d0
André Leandro Vieira <134546903+souandrevieira@users.noreply.github.com>
2023-11-10 12:02:46 -0300

    Add files via upload

--- index.py
+++ index.py
@@ -26,8 +26,8 @@ def ui():
                         <div class="card" style="width: 18rem;">
                         <img class="card-img-top" src="{infousuario['avatar_url']}" alt="Card image cap">
                         <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <h5 class="card-title">{infousuario['login']}</h5>
+                            <p class="card-text">{infousuario['bio']}</p>
                             <a href="#" class="btn btn-primary">Go somewhere</a>
                         </div>
                         </div>
